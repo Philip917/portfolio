a641de9c784a7646e1b309ad8ac168968fef8f2d
Philip Koekemoer <philips.progdev@gmail.com>
2019-10-15 08:53:23 +0200

    Updates

---
+++ precache-manifest.dea447d7d892c6ce9b3e61b57c93324a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673c7a8d657eece902cfc492dcd6b07c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bc3094a3a64829d0eeca",
+    "url": "/portfolio/static/css/main.1bb94c6f.chunk.css"
+  },
+  {
+    "revision": "fce24a4baa6a695afca8",
+    "url": "/portfolio/static/js/2.b9f323cf.chunk.js"
+  },
+  {
+    "revision": "bc3094a3a64829d0eeca",
+    "url": "/portfolio/static/js/main.956be610.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "a0fd09e252e724bb523cc68bc147ed3a",
+    "url": "/portfolio/static/media/b20.a0fd09e2.jpeg"
+  }
+]);
